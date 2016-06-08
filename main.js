3127b706dbb835f9920c890f0e54908f6b25fad1
elliothimmelfarb <elliothimmelfarb@gmail.com>
2016-06-07 23:05:46 -0700

    second commit

---
+++ main.js
@@ -0,0 +1,122 @@
+document.addEventListener('DOMContentLoaded', init);
+
+var nameList = [];
+// var teams = [];
+// var randomName = '';
+
+function init() {
+   document.querySelector('.add-names').addEventListener('click', addNames);
+   document.querySelector('.clear-names').addEventListener('click', clearNames);
+   document.querySelector('.pick-random').addEventListener('click', pickRandom);
+   document.querySelector('.make-teams').addEventListener('click', makeTeams);
+}
+
+function addNames() {
+   var names = document.querySelector('#name-box').value.split(',').filter(clean);
+
+   function clean(e) {
+      return e && e !== " ";
+   }
+   for (var i = 0; i < names.length; i++) {
+      var name = document.createElement('span');
+      name.classList.add('name');
+      var text = document.createTextNode(names[i] + ' ');
+      name.appendChild(text);
+      document.querySelector('.name-display').appendChild(name);
+      nameList.push(names[i]);
+   }
+   document.querySelector('#name-box').value = '';
+
+   // make sure number picker can't make teams larger than number of people
+   document.querySelector('#teams-number').setAttribute('max', nameList.length);
+
+   console.log(nameList);
+}
+
+function clearNames() {
+   document.querySelector('.name-display').innerHTML = '';
+   nameList = [];
+   console.log(nameList)
+}
+
+function pickRandom() {
+   document.querySelector('.random-display').innerHTML = '';
+   var random = Math.floor(Math.random() * nameList.length);
+   var name = document.createElement('span');
+   var text = document.createTextNode(nameList[random]);
+   name.appendChild(text);
+   document.querySelector('.random-display').appendChild(name);
+}
+
+function makeTeams() {
+   var teamSize = document.querySelector('#teams-number').value;
+   //console.log('teamSize:', teamSize);
+   var numOfTeams = Math.ceil(nameList.length / teamSize);
+   //console.log('numOfTeams:', numOfTeams);
+   var shufArr = shuffle(nameList);
+   console.log(shufArr);
+   var teams = [];
+
+   for (var i = 0; i < numOfTeams; i++) {
+      //console.log('team:' + (i + 1));
+      var card = document.createElement('ul');
+      card.classList.add('team-list');
+      //console.log('card classList:', card.classList);
+      var li = document.createElement('li');
+      //console.log('li:', li);
+      li.classList.add('team-header');
+      //console.log('li classList:', li.classList);
+      li.textContent = 'team:' + (i + 1);
+      card.appendChild(li);
+      //console.log(card);
+      for (var j = 0; j < teamSize; j++) {
+         //console.log("student");
+         var student = document.createElement('li');
+         student.textContent = shufArr.pop();
+         //console.log('student:', student);
+         card.appendChild(student);
+
+      }
+      //console.log('card:', card);
+      document.querySelector('.team-display').appendChild(card);
+   }
+
+   /*
+      var num = document.querySelector('#teams-number').value;
+      console.log('num:', num);
+      var times = Math.ceil(nameList.length / num)
+      console.log("times:", times)
+      nameIndex = 0
+      for (var i = 0; i <= times; i++) {
+         var team = document.createElement('div');
+         var p = document.createElement('p');
+         p.textContent = "Team " + (i + 1) + ":";
+         console.log(p);
+         team.appendChild(p);
+         console.log("team early:", team);
+         for (var i = 0; i < num; i++) {
+            if (nameIndex <= nameList.length) {
+               var n = document.createElement('p');
+               n.textContent = nameList[nameIndex];
+               console.log("n:", n);
+               team.appendChild(n);
+               console.log("team final:", team)
+               nameIndex++;
+            }
+         }
+         document.querySelector('.team-display').appendChild(team);
+      } */
+}
+
+function shuffle(arr) {
+   var result = [];
+   var used = [];
+   while (result.length < nameList.length) {
+      var random = Math.floor(Math.random() * nameList.length);
+      if (used.indexOf(random) === -1) {
+         result.push(nameList[random]);
+         used.push(random);
+      }
+   }
+   return result;
+}
